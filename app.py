e3567aff81e830d81c488395ace1cd465d370535
Sanjay Kotabagi <sanjaymkotabagi@gmail.com>
2024-04-03 03:28:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,5 @@ import streamlit as st
 import webbrowser 
 
 url = st.text_input(label="Enter URL")
-res = webbrowser.open(url)
 st.write("Results for : ",url)
-st.write(res)
+st.write(webbrowser.open(url))
